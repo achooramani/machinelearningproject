2d4225ad8fb4acd4e3342782cf65d0609645e5cb
afreenshakeel24 <afreen.ubc@gmail.com>
2021-04-18 12:16:51 -0700

    parse

--- data/pre_processing_code/parse_code.py
+++ data/pre_processing_code/parse_code.py
@@ -14,6 +14,7 @@ pid1=[]
 level1=[]
 component1=[]
 content1=[]
+type_of_content1=[]
 
 parsed_data=[]
 
@@ -29,6 +30,7 @@ with open("data/raw_data/HDFS/HDFS_2k.log","r") as file:
         component = before_colon.split()[4]
 
         content = line.split(':',1)[1].strip() #after colon
+        type_of_content = (line.split(':',1)[1].strip()).split(' ', 1)[0]
 
         date1.append(date)
         time1.append(time)
@@ -36,11 +38,35 @@ with open("data/raw_data/HDFS/HDFS_2k.log","r") as file:
         level1.append(level)
         component1.append(component)
         content1.append(content)
+        type_of_content1.append(type_of_content)
 
-    parsed_data = pd.DataFrame({'Date':date1, 'Time':time1, 'PID':pid1, 'Level':level1, 'Component':component1, 'Content':content1})
-    #parsed_data.columns.values[0] = "LineID"
-    print(parsed_data)
+    parsed_data = pd.DataFrame({'Date':date1, 'Time':time1, 'PID':pid1, 'Level':level1, 'Component':component1, 'Content':content1, 'TypeOfContent': type_of_content1})
+    parsed_data['datetime_id'] = parsed_data['Date']+parsed_data['Time']
+    parsed_data['Tag'] = parsed_data['TypeOfContent']
 
+    parsed_data1 = parsed_data[(parsed_data['Tag'] == "PacketResponder") | 
+                               (parsed_data['Tag'] == "BLOCK*") |
+                               (parsed_data['Tag'] == "Received") |  
+                               (parsed_data['Tag'] == "Receiving") |
+                               (parsed_data['Tag'] == "Verification") |
+                               (parsed_data['Tag'] == "Deleting")]
+    parsed_data2 = parsed_data[(parsed_data['Tag'] != "PacketResponder") & 
+                               (parsed_data['Tag'] != "BLOCK*") &
+                               (parsed_data['Tag'] != "Received") &  
+                               (parsed_data['Tag'] != "Receiving") &
+                               (parsed_data['Tag'] != "Verification") &
+                               (parsed_data['Tag'] != "Deleting")]
+    
+    parsed_data2_GOT = parsed_data2[parsed_data2['Tag'].str.contains('Got')]
+    parsed_data2_GOT['Tag'] = "Others_IP_WARN"
+
+    parsed_data2_non_GOT = parsed_data2[-parsed_data2['Tag'].str.contains('Got')]
+    parsed_data2_non_GOT['Tag'] = "Others_IP_INFO"
+
+    frames = [parsed_data1, parsed_data2_GOT, parsed_data2_non_GOT]
+
+    parsed_data = pd.concat(frames)
+    
     parsed_data.to_csv('data/processed_data/HDFS/HDFS_2k_structured.csv', index=False)
 
  
